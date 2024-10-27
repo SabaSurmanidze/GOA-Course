7065cc8eaf94fdecd8169249617106026c998c98
saba surmanidze <surmanidzemerab2@gmail.com>
2024-10-27 18:53:47 +0400

    commit

---
+++ Day 35/Homework/task5.py
@@ -0,0 +1,9 @@
+#5. შექმენით ფუნქცია, რომელიც იღებს string'ს და აბრუნებს True-ს თუ
+#  ის არის Palindrome (ანუ იგივენაირად იკითხება მარცნიდანაც და მარჯვნიდანაც 
+# მაგალითად: "wow") და სხვა შემთხვევაში False-ს.
+
+string = input("enter palindrome:")
+if string == "palindrome":
+    print(True)
+else:
+    print(False)    
