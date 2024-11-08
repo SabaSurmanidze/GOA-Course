e9bc9c82eb97db4fba33c0c5e2c0066e8582be45
saba surmanidze <surmanidzemerab2@gmail.com>
2024-11-08 15:49:11 +0400

    commit

---
+++ Day 41/Classwork/cw1.py
@@ -0,0 +1,13 @@
+
+list = ["water","apple","banana","pinnaple","coke"]
+
+index = int(input("enter index numbers 0 to 4:"))
+
+if index > 4:
+    print("choose higher number than 0")
+elif index<-5:
+    print("choose higher  number  than -5")
+else:
+    print(list[index])
+    
+    
