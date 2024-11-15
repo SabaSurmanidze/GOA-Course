543e51f6705fbde855fdbb5dd3e6a8da07f2775f
saba surmanidze <surmanidzemerab2@gmail.com>
2024-11-15 14:13:32 +0400

    commit

---
+++ Day 43/Homework/task5.py
@@ -0,0 +1,6 @@
+# 5. შექმენით სია, 5 სტრინგით, შემდეგ .append()-ის მეშვეობით სიის ბოლოში დაამატეთ სასურველი სიტყვა.
+
+
+list = ["banana","water","pinapple","apple","fanta"]
+list.append("cola")
+print(list)
---
+++ Day 43/Homework/task6.py
@@ -0,0 +1,6 @@
+# 6. შექმენით სია, 5 სტრინგით, შემდეგ .insert()-ის მეშვეობით სიაში სასურველ ინდექსზე დაამატეთ სასურველი სიტყვა.
+
+list = ["banana","water","pinapple","apple","fanta"]
+
+list.insert(2,"cola")
+print(list)
