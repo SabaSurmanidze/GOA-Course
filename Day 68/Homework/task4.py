d60966349831696e6ef32d46d4f77aa039fe065e
saba surmanidze <surmanidzemerab2@gmail.com>
2025-02-07 18:52:36 +0400

    commit

---
+++ Day 68/Homework/task4.py
@@ -0,0 +1,7 @@
+# 4. Create a function that takes a list and a list of items, and appends each item to the original list.
+
+
+def function(list , new_list):
+   list.append(new_list)
+   print(list)
+function([1,2,3] , [4,5,6])
---
+++ Day 68/Homework/task5.py
@@ -0,0 +1,6 @@
+# 5. Create a function that appends all elements of one list to another list.
+
+def function(new_list , list):
+   new_list.append(list)
+   print(new_list)
+function([1,2,3] , [4,5,6])
