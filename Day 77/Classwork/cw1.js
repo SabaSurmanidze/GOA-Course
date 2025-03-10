919862036286ed8f9cfa3dcb065db6238c963dff
saba surmanidze <surmanidzemerab2@gmail.com>
2025-03-10 15:12:52 +0400

    commit

---
+++ Day 77/Classwork/cw1.js
@@ -0,0 +1,14 @@
+function borderRadius(){
+    let image = document.getElementById('poster')
+    image.style.borderRadius = '15px'
+}
+
+function boxShadow(){
+    let image = document.getElementById('poster')
+    image.style.boxShadow = "0px 0px 10px 2px black"
+}
+
+function changeImage(){
+    let image = document.getElementById('poster')
+    image.src = "images/image2.jpg"
+}
