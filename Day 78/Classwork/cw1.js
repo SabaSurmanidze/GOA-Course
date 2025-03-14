7f68a9c3325291184c59fc8c2af88595469e4682
saba surmanidze <surmanidzemerab2@gmail.com>
2025-03-14 17:43:56 +0400

    commit

---
+++ Day 78/Classwork/cw1.js
@@ -0,0 +1,7 @@
+function clickbtn(){
+    let input  = document.getElementById("input")
+    let h3  = document.getElementById("h3")
+    h3.textContent = input.value
+    console.log(input.value)
+
+}
---
+++ Day 78/Homework/task1.js
@@ -0,0 +1,6 @@
+
+function inputField(){
+    input = prompt("Enter your name:")
+    alert("hello " + input)
+}
+inputField()
