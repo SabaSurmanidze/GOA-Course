fd5468226fc2dad59b63e71262d500521050ba8d
saba surmanidze <surmanidzemerab2@gmail.com>
2025-03-18 19:42:21 +0400

    commit

---
+++ Day 79/Classwork/cw1.js
@@ -0,0 +1,8 @@
+
+
+function alerts() {
+    let input = prompt("Enter your name:")
+    alert(input)
+    confirm("quit the website")
+}
+alerts()
---
+++ Day 79/Homework/task1.js
@@ -0,0 +1,19 @@
+function math(){
+    let number1 = 5
+    let number2 = 10
+    console.log(number1 + number2)
+    console.log(number1 > number2)
+    console.log( number1 === number2)
+    console.log( number1 < number2)
+}
+math()
+function comfirm() {
+    let comfirm = confirm("Quit the website?")
+    if (comfirm == true){
+        console.log("you will leave this site soon")
+    }else{
+        console.log("you are staying in the site")
+    }
+}
+comfirm()
+
