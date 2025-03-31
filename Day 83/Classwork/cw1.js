6085a7c3aadb86eb625366372b75551b2917d146
saba surmanidze <surmanidzemerab2@gmail.com>
2025-03-31 18:32:00 +0400

    commit

---
+++ Day 83/Classwork/cw1.js
@@ -0,0 +1,14 @@
+function type() {
+    let str = "saba"
+    let number = 8
+    let boolean = true
+    console.log(typeof str)
+    console.log(typeof number)
+    console.log(typeof boolean)
+}
+
+type()
+
+// sequencing არის თანმიმდევრობა მაგალითად რომ გქონდეს დალაგებული რას გააკეთებ დღეს
+// iteration არის განმორება მაგალითად აკეთებდე ყოველდღე ერთიდაიგივეს
+// selection არის არჩევა მაგალითად გქონდეს ორი არჩევანი თუ რა ჭამო დილით
