ec0e7ba8993eee83d2b8b53ea250debee9a8c1a2
saba surmanidze <surmanidzemerab2@gmail.com>
2025-06-23 13:56:42 +0400

    commit

---
+++ Day 106/Classwork/script.js
@@ -0,0 +1,8 @@
+// 1. შექმენით მასივი, სადაც გექნებათ ყველა ინფორმაციის ტიპი, შემდეგ გამოიტანეთ თითოეული მათგანი ინდექსინგის გამოყენებით.
+
+const array = ["string" , 13 , true , 16.5]
+
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
