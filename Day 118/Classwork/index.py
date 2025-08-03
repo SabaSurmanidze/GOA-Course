5ffe5e169c3156483371283562f9adc05a931805
saba surmanidze <surmanidzemerab2@gmail.com>
2025-08-03 18:06:28 +0400

    commit

---
+++ Day 118/Classwork/index.py
@@ -0,0 +1,10 @@
+# 2) გააკეთეთ 3-3 მაგალითი index და count მეთოდებზე
+
+list = [1,2,3,34,10,3,334,56,7,8,2,34,5,6,7,1,2,3,4,5,6,7,8]
+print(list.count(1))
+print(list.count(2))
+print(list.count(3))
+
+print(list.index(7))
+print(list.index(4))
+print(list.index(5))
