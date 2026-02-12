bad2953b7f94e3105126be8093f202ade80667e4
saba surmanidze <surmanidzemerab2@gmail.com>
2026-02-12 20:33:20 +0400

    commit

---
+++ Day 171/Homework/script.js
@@ -0,0 +1,58 @@
+const title = document.getElementById("title")
+const note = document.getElementById("note")
+const preview = document.querySelector(".preview")
+const tags = document.querySelectorAll(".tag")
+const notePreview = document.querySelector(".note-preview")
+const addNote = document.getElementById("Add")
+let titleh1 = document.getElementById("title-h1")
+let noteh1 = document.getElementById("note-p")
+let tagp = document.getElementById("tag-h2")
+let targetValue = ""
+
+
+tags.forEach(tag => {
+    tag.addEventListener("click" , (e) => {
+        targetValue = e.target.textContent
+    })
+})
+
+
+// <div class="note-preview">
+//         h1 id="title-h1"></h1>
+//     p id="note-p"></p>
+//     <h4 id="tag-h2"></h4>
+//     </div>
+
+addNote.addEventListener("click" , () => {
+
+
+    
+
+    notePreview.style.display = "flex"
+
+    const objectNote = {
+            title: title.value,
+            note: note.value,
+            tags: targetValue
+    }
+
+    if (objectNote.title == "" && objectNote.note == "" && objectNote.tags =="" ) {
+        notePreview.style.display = "none"
+    }
+
+        console.log(objectNote)
+        titleh1.textContent =  `Tittle : ${objectNote.title}`
+        noteh1.textContent = ` Description : ${objectNote.note}`
+        tagp.textContent = objectNote.tags
+
+    title.value = ""
+    note.value = ""
+    targetValue = ""
+})
+
+
+
+
+
+
+
